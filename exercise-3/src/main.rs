5e1c3e31a847e6efe0946aec57d85295b5b53d8c
epi <43392618+epi052@users.noreply.github.com>
2025-02-14 06:29:18 -0500

    fixed up things in ex3 for 15.1

--- exercise-3/src/main.rs
+++ exercise-3/src/main.rs
@@ -1,13 +1,14 @@
 mod parser;
 
-use libafl::bolts::core_affinity::Cores;
+use libafl_bolts::core_affinity::Cores;
 use libafl_sugar::ForkserverBytesCoverageSugar;
 
 fn main() {
     let parsed_opts = parser::parse_args();
     let cores = Cores::from_cmdline(&parsed_opts.cores).expect("Failed to parse cores");
 
-    ForkserverBytesCoverageSugar::<86217>::builder()
+    // map size of 98_816 required on my machine
+    ForkserverBytesCoverageSugar::builder()
         .input_dirs(&[parsed_opts.input])
         .output_dir(parsed_opts.output)
         .cores(&cores)
